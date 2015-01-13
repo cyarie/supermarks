0e5ee798699df81e5346f29fbcdc98c9eb008517
cyarie <cyarie@gmail.com>
2015-01-13 18:56:35 -0500

    Adding in our first models!

--- supermarks_app/models.py
+++ supermarks_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BookMarks(models.Model):
+    bookmark_id = models.AutoField(primary_key=True)
+    long_url = models.CharField(max_length=255)
+    short_url = models.CharField(max_length=255)
+    create_dt = models.DateTimeField(auto_now_add=True)
+    mod_dt = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tags, related_name='bookmarks')
+
+
+class Tags(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=255)
+    create_dt = models.DateTimeField(auto_now_add=True)
+    mod_dt = models.DateTimeField(auto_now=True)
+
+
+class MarkUsers(models.Model):
+    user = models.OneToOneField(User)
+    bookmarks = models.ManyToManyField(BookMarks, related_name='bookmarks')
+
+
