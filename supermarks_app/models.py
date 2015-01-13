1cbbb5c4a93b1c80e28dfa7183589f1047bf51c4
cyarie <cyarie@gmail.com>
2015-01-13 18:57:56 -0500

    Because it was out of order. Dumb.

--- supermarks_app/models.py
+++ supermarks_app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Tags(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=255)
+    create_dt = models.DateTimeField(auto_now_add=True)
+    mod_dt = models.DateTimeField(auto_now=True)
+
+
 class BookMarks(models.Model):
     bookmark_id = models.AutoField(primary_key=True)
     long_url = models.CharField(max_length=255)
@@ -9,13 +16,6 @@ class BookMarks(models.Model):
     create_dt = models.DateTimeField(auto_now_add=True)
     mod_dt = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tags, related_name='bookmarks')
-
-
-class Tags(models.Model):
-    tag_id = models.AutoField(primary_key=True)
-    tag = models.CharField(max_length=255)
-    create_dt = models.DateTimeField(auto_now_add=True)
-    mod_dt = models.DateTimeField(auto_now=True)
 
 
 class MarkUsers(models.Model):
