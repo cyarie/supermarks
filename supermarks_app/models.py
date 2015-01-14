3640db5ddf3e5154bae3e06f69aaf74b794d5180
cyarie <cyarie@gmail.com>
2015-01-13 21:21:29 -0500

    Return sensical things for our models, also added a title field for bookmarks

--- supermarks_app/models.py
+++ supermarks_app/models.py
@@ -8,18 +8,28 @@ class Tag(models.Model):
     create_dt = models.DateTimeField(auto_now_add=True)
     mod_dt = models.DateTimeField(auto_now=True)
 
+    def __unicode__(self):
+        return self.tag
+
 
 class BookMark(models.Model):
     bookmark_id = models.AutoField(primary_key=True)
     long_url = models.CharField(max_length=255)
     short_url = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     create_dt = models.DateTimeField(auto_now_add=True)
     mod_dt = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField(Tag, related_name='bookmarks')
+
+    def __unicode__(self):
+        return self.title
 
 
 class MarkUser(models.Model):
     user = models.OneToOneField(User)
     bookmarks = models.ManyToManyField(BookMark, related_name='bookmarks')
+
+    def __unicode__(self):
+        return self.username
 
 
