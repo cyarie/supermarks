28dd38d771632e8ee06801afb010db1c6496d3a7
cyarie <cyarie@gmail.com>
2015-01-14 13:21:05 -0500

    Reading some of the docs and rewriting that custom create() method

--- supermarks_app/serializers.py
+++ supermarks_app/serializers.py
@@ -32,10 +32,10 @@ class MarkUserSerializer(serializers.HyperlinkedModelSerializer):
 
     def create(self, validated_data):
         user = User(
-            username=validated_data.user.username,
-            email=validated_data.user.email,
-            first_name=validated_data.user.first_name,
-            last_name=validated_data.user.last_name
+            username=validated_data['user']['username'],
+            email=validated_data['user']['email'],
+            first_name=validated_data['user']['first_name'],
+            last_name=validated_data['user']['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
