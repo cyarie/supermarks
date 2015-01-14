74b8ebeedd3803cde947d716b26af5026e9a3572
cyarie <cyarie@gmail.com>
2015-01-13 21:07:48 -0500

    Redoing the model names

--- supermarks_app/models.py
+++ supermarks_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Tags(models.Model):
+class Tag(models.Model):
     tag_id = models.AutoField(primary_key=True)
     tag = models.CharField(max_length=255)
     create_dt = models.DateTimeField(auto_now_add=True)
     mod_dt = models.DateTimeField(auto_now=True)
 
 
-class BookMarks(models.Model):
+class BookMark(models.Model):
     bookmark_id = models.AutoField(primary_key=True)
     long_url = models.CharField(max_length=255)
     short_url = models.CharField(max_length=255)
@@ -18,7 +18,7 @@ class BookMarks(models.Model):
     tags = models.ManyToManyField(Tags, related_name='bookmarks')
 
 
-class MarkUsers(models.Model):
+class MarkUser(models.Model):
     user = models.OneToOneField(User)
     bookmarks = models.ManyToManyField(BookMarks, related_name='bookmarks')
 
