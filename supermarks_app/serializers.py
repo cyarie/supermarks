36a4486892c5ace40e385838f5fa07d41b09f98e
cyarie <cyarie@gmail.com>
2015-01-14 13:55:46 -0500

    I hate djangrest right now

--- supermarks_app/serializers.py
+++ supermarks_app/serializers.py
@@ -18,24 +18,9 @@ class TagSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class MarkUserSerializer(serializers.HyperlinkedModelSerializer):
-    username = serializers.CharField(source='user.username')
-    email = serializers.CharField(source='user.email')
-    password = serializers.CharField(source='user.password')
-    first_name = serializers.CharField(source='user.first_name')
-    last_name = serializers.CharField(source='user.last_name')
     bookmarks = serializers.HyperlinkedRelatedField(many=True, view_name='bookmark-detail', read_only=True)
 
     class Meta:
-        model = MarkUser
+        model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'bookmarks')
         extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        markuser = MarkUser()
-        markuser.user.username = validated_data['user']['username']
-        markuser.user.email = validated_data['user']['email']
-        markuser.user.first_name = validated_data['user']['first_name']
-        markuser.user.last_name = validated_data['user']['last_name']
-        markuser.user.set_password(validated_data['user']['password'])
-        markuser.save()
-        return markuser
